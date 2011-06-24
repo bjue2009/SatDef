8b80f508bda025f26f71e1e885e8db63517d601b
Brandon <jue.brandon@gmail.com>
2011-06-24 14:01:21 -0700

    Files to hold how 3DPolar Coordinates will work, translating them to the viewing rectangle

---
+++ SatDef/Polar.h
@@ -0,0 +1,16 @@
+//
+//  Polar.h
+//  SatDef
+//
+//  Created by Brandon Jue on 6/24/11.
+//  Copyright 2011 bjued. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Polar : NSObject {
+    
+}
+
+@end
---
+++ SatDef/Polar.m
@@ -0,0 +1,14 @@
+//
+//  Polar.m
+//  SatDef
+//
+//  Created by Brandon Jue on 6/24/11.
+//  Copyright 2011 bjued. All rights reserved.
+//
+
+#import "Polar.h"
+
+
+@implementation Polar
+
+@end
